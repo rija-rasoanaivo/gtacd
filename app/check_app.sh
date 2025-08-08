85a9dd19d41ce2b850244aa0b7c594a7e33973da
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2025-08-08 12:33:43 +0200

    Ajout du fichier YAML pour GitHub Actions

--- app/check_app.sh
+++ app/check_app.sh
@@ -4,6 +4,8 @@ source var.sh
 
 if [ -f "$FILE_PATH" ]; then
     echo "0"
+    exit 0
 else
     echo "1"
+    exit 1
 fi
---
+++ app/var.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+FILE_PATH="C:\Users\rijar\Desktop\Ecole_Laplateforme\Projet\gtacd\app\index.html"
